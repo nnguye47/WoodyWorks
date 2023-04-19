d98a9eaaf928469ada5d2765ba702f2d1ec376ce
Nam Nguyen <nam.nguyen201@gmail.com>
2023-04-18 18:26:44 -0700

    open a modal when an image is clicked

--- client/src/Art/Product.jsx
+++ client/src/Art/Product.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import styled from 'styled-components';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Prod = styled.div`
   margin: 10px;
@@ -31,10 +33,29 @@ const style = {
 };
 
 export default function Product() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Prod>
-      <button type="button">
+      <button type="button" onClick={handleOpen}>
         <Image src="./assets/logo.png" alt="product" height="200px" width="200px" />
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <button type="button" onClick={handleClose}>
+              close modal
+            </button>
+          </Box>
+        </Modal>
       </button>
     </Prod>
   );
